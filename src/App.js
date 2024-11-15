5a91eeb95d836f3090e5ea89376ce1547375d43c
Sherman Fakhimi <shermf7@yahoo.com>
2024-11-15 02:42:25 -0600

    Homepage fix real

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />
